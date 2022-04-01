a6780653c920976c31403db18f796fca30cb8db2
GamingLiamStudios <58615717+GamingLiamStudios@users.noreply.github.com>
2022-04-01 14:20:07 +1100

    Move library search to module

---
+++ .cmake/find_libraries.cmake
@@ -0,0 +1,34 @@
+include(FetchContent)
+
+# Include zlib-ng
+set(ZLIB_ENABLE_TESTS OFF)
+set(WITH_NATIVE_INSTRUCTIONS ON)
+
+FetchContent_Declare(
+        zlib
+        GIT_REPOSITORY https://github.com/zlib-ng/zlib-ng
+        GIT_TAG        2.0.6
+)
+
+FetchContent_MakeAvailable(zlib)
+
+find_package(CURL)
+if(NOT CURL_FOUND)
+    FetchContent_Declare(
+            curl
+            GIT_REPOSITORY https://github.com/curl/curl
+            GIT_TAG        curl-7_80_0
+    )
+    FetchContent_MakeAvailable(curl)
+endif()
+
+# find_package in main CMakeLists because it doesn't work in here for some reason
+find_package(json-c CONFIG)
+if(NOT json-c_FOUND)
+    FetchContent_Declare(
+            json-c
+            GIT_REPOSITORY https://github.com/json-c/json-c
+            GIT_TAG        json-c-0.15
+    )
+    FetchContent_MakeAvailable(json-c)
+endif()
